32ce269b51ebdae4ffdd7c4b5d30a4c3d6543860
diatinkle@gmail.com <diatinkle@gmail.com>
2021-08-24 16:21:52 +1200

    1st commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+
+
+'''
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("_JST",["_JST.pyx"], include_dirs=[np.get_include()])]
+)
+'''
+
+extensions = [
+    Extension("_JST", ["_JST.pyx"],
+        include_dirs=[np.get_include()]
+              )
+]
+setup(
+    name="_JST",
+    ext_modules=cythonize(extensions)
+)
